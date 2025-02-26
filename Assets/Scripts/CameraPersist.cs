36858ae8833f521d35c5c677a60a68b6d2f128d7
Mitchell Thai <mitchthai.2004@gmail.com>
2025-02-26 11:18:35 -0500

    #7 Makes the player and camera (#8) go between scenes
    Player doesn't spawn in the correct location

---
+++ Assets/Scripts/CameraPersist.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraPersist : MonoBehaviour
+{
+    private static CameraPersist instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
