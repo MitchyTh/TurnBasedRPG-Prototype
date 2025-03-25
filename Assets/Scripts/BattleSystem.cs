380ed3d15767190e5d55a3a65c5d72c960146b94
adelinebelova <63752238+adelinebelova@users.noreply.github.com>
2025-03-25 16:03:09 -0400

    #38 Setup BattleSystem Script

---
+++ Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using TMPro;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+public class BattleSystem : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    public TextMeshProUGUI dialogueText;
+    public BattleState state;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        state = BattleState.START;
+        SetupBattle();
+    }
+
+    void SetupBattle(){
+        GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+        playerUnit = playerGO.GetComponent<Unit>();
+
+        GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+
+        dialogueText.text = enemyUnit.unitName + " approaches!";
+    }
+
+}
